d798800ee1ca87d04669af83ee5d1f6507db910b
Pokidj <Javipoki@gmail.com>
2026-04-15 13:26:49 +0200

    Update script.js

--- edit/script.js
+++ edit/script.js
@@ -180,7 +180,7 @@ document.getElementById("modal").style.display="flex";
 
 const select=document.getElementById("category");
 const newCat=document.getElementById("newCategory");
-  
+
 select.innerHTML="";
 Object.keys(data).forEach(c=>select.innerHTML+=`<option>${c}</option>`);
 
@@ -197,20 +197,12 @@ select.value=section;
 item.links.forEach(l=>addLinkField(l));
 
 newCat.style.display="none"; // 🔥 ocultar en editar
-newCat.style.height="0";
-newCat.style.padding="0";
-newCat.style.margin="0";
-newCat.style.border="none";
 
 editTarget={section,name};
 }else{
 document.getElementById("modalLogo").src="/ROMDOCK/img/default.png";
 document.getElementById("name").value="";
-newCat.style.display="block";// 🔥 mostrar en crear
-newCat.style.height="";
-newCat.style.padding="";
-newCat.style.margin="";
-newCat.style.border=""; 
+newCat.style.display="block"; // 🔥 mostrar en crear
 
 addLinkField();
 editTarget=null;
